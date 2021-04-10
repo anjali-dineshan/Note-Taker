a0232454bb4c04384f0810c5c484740e8f1d6169
anjali <anjalidinesh97@gmail.com>
2021-04-10 21:18:00 +0530

    second commit

--- script.js
+++ script.js
@@ -1,45 +1,97 @@
+let count=1;
 function addNote()
 {
     let note=document.getElementById("note").value;
-    let divId=document.getElementById("notes-body");
-
+    let divId=document.getElementById("notes-body"); //Parent Div
    let div=document.createElement("div");
-   divId.append(div);
-    let count=1;
-   div.innerHTML=`<p>
-   <span>Note</span>
-   <span>${count}</span>
-   </p>
-   <p class="text-note">${note}</p>
-   <a class="detail-button">View Detail</a>`
-   count=count+1;
+   div.setAttribute("id",count);
+
+   if(note.length>=180)
+   {
+    let substr=note.substring(0,180);
+    let substr1=note.substr(180);
+    div.innerHTML=`<p>
+            <span>Note</span>
+            <span>${count}</span>
+            </p>
+            <p class="text-note" id="NoteP">${substr}...</p>
+            <a class="detail-button" onclick="popup(this.parentElement,${substr1})">View Detail</a>`;
+   }
+   else
+   {
+    div.innerHTML=`<p>
+    <span>Note</span>
+    <span>${count}</span>
+    </p>
+    <p class="text-note" id="NoteP">${note}</p>
+    <a class="detail-button">View Detail</a>`;
+   }
+    divId.append(div);
+    count++;
 
    //popup
-   var detailButton = document.getElementsByClassName("detail-button");
-   console.log(detailButton)
-   for (var i = 0; i < detailButton.length; i++) 
-    {
-        detailButton[i].addEventListener('click', popup); 
-    }
+//    var detailButton = document.getElementsByClassName("detail-button");
+//    for (var i = 0; i < detailButton.length; i++) 
+//     {
+//         detailButton[i].addEventListener('click', popup); 
+//     }
+
+    
+    
+ 
 }
 //pop up function
-function popup()
+function popup(parentDiv, st)
 {
+    console.log("hai")
     document.getElementById("popup-div").classList.remove("hide");
 
-    let note=document.getElementById("note").value;
-    let divId=document.getElementById("popup-div");
-    let div=document.createElement("div");
-    divId.appendChild(div);
-    div.innerHTML=`<div class="overlay">
-                        <div class="content">
-                            <div class="close-btn" onclick="closeFun()">&times;</div>
-                            <p></p>${note}</div></div>`;
 
+    let NoteP=parentDiv.childNodes[2].innerHTML; //to get 3rd element in the div 
+    console.log(parentDiv)
+    // console.log(substr1)
+
+
+
+
+
+
+
+
+
+
+
+    // let divId=document.getElementById("popup-div");
+    // let div=document.createElement("div");
+    // if(note.length>=180)
+    // {
+    //     // let substr=note.substring(0,180);
+    //     // let substr1=note.substr(180);
+    //     let fullString=substr+substr1;
+    //     console.log(fullString);
+    
+    //     div.innerHTML=`<div class="overlay">
+    //     <div class="content">
+    //         <div class="close-btn" onclick="closeFun()">&times;</div>
+    //         <p></p>${fullString}</div></div>`;
+    // }
+    // else
+    // {
+    //     console.log("hai")
+    //     div.innerHTML=`<div class="overlay">
+    //     <div class="content">
+    //         <div class="close-btn" onclick="closeFun()">&times;</div>
+    //         <p></p>${NoteP}</div></div>`;
+    // }
+ 
+
+    // divId.appendChild(div);
+    
 }
 //close btn
 function closeFun()
 {
     document.getElementById("popup-div").classList.toggle("hide");
 }
+
 
